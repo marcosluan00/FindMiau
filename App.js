9ec8e1a6f647b48fdbe7c003eef4ce6f3230f821
marcosluan00 <luansousa9141@gmail.com>
2021-12-14 16:01:16 -0300

    Alteracoes c

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, 
+  TextInput, 
+  StyleSheet, 
+  KeyboardAvoidingView, 
+  Image,
+  TouchableOpacity,
+  Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View>
+        <Image source={require('./assets/logo.png')}/>
+      </View>
+
+      <View>
+      <TextInput
+      placeholder="Email"
+      autoCorrect={false}
+      onChangeText={() =>{}}
+      />
+      <TextInput
+      placeholder="Senha"
+      autoCorrect={false}
+      onChangeText={() =>{}}
+      />
+      <TouchableOpacity>
+        <Text> Acessar </Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text> Cadastrar-se a si mesmo </Text>
+      </TouchableOpacity>
+      
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
