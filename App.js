b8eea94ac2b8832e95d9fb663f669f4a2d9824aa
marcosluan00 <luansousa9141@gmail.com>
2022-02-04 10:25:58 -0300

    Refatoração dos components com Native Base

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import Cadastro from './src/Pages/Cadastro';
 import Esqueci from './src/Pages/Esqueci';
 import Home from './src/Pages/Home';
 import Modal from './src/Pages/Modal';
+import NativeBase from './src/Pages/NativeBase';
+
 
 export default function App() {
     return (
-    <Home /> 
+    <NativeBase /> 
     
     );
 }
